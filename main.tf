0e72b89c01d1cb1d34883142a74a67c9ab3bb0aa
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-03-05 20:46:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-/*# Configure le fournisseur AWS
+# Configure le fournisseur AWS
 provider "aws" {
   region     = "us-west-2"  # Remplacez par la région de votre choix
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-*/
+
 
 # Crée un bucket S3
 resource "aws_s3_bucket" "my_bucket" {
