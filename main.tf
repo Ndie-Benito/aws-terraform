fa93fe4211f4ed6384e80de12702d3bd93520edc
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-03-07 18:49:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,32 +8,12 @@ provider "aws" {
 
 # Crée un bucket S3
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name  # Utilise la variable pour le nom du bucket
- 
-
+  count = 5 
+  bucket = "my-bucket-${count.index + 1}" 
   tags = {
-    Name        = "MonBucket"
-    Environment = "Production"
-  }
-}
-/*
-# Optionnel : Activer la versioning sur le bucket
-resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"  # Active la versioning
+    Name        = "My Bucket ${count.index + 1}"
+    Environment = "production"
   }
 }
 
-# Optionnel : Bloquer l'accès public au bucket
-resource "aws_s3_bucket_public_access_block" "my_bucket_public_access" {
-  bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-*/
